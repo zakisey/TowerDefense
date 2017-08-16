5647b3ca8ce0720efeda9ff346fa448b791bfb6e
CHEUK WA CHRISTOPHER LO <christopher.lo@avantcorp.com>
2017-08-16 17:49:26 +0900

    Stage Selection

---
+++ Tower Defense/Assets/Scripts/StageButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageButton : MonoBehaviour {
+
+    public bool locked = false;
+    public 
+
+	// Use this for initialization
+	void Start () {
+		if (locked)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
