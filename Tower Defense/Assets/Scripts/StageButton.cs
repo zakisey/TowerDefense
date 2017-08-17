d6124225dd23bfac997092f53af0b940ba53c370
CHEUK WA CHRISTOPHER LO <christopher.lo@avantcorp.com>
2017-08-17 10:25:19 +0900

    StageSelection

--- Tower Defense/Assets/Scripts/StageButton.cs
+++ Tower Defense/Assets/Scripts/StageButton.cs
@@ -1,22 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class StageButton : MonoBehaviour {
+public class StageButton : MonoBehaviour
+{
+    public GameObject lockPicture;
+    public GameObject starPicture;
 
-    public bool locked = false;
-    public 
+    public bool locked;
+    public string stage;
+    public int starNumber;
 
-	// Use this for initialization
-	void Start () {
-		if (locked)
+    // Use this for initialization
+    void Start()
+    {
+        if (locked)
         {
-
+            this.GetComponent<Button>().interactable = false;
+            Instantiate(lockPicture, this.transform);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        else
+        {
+            float xAdjustment = -this.transform.GetComponent<RectTransform>().rect.width / 4;
+            float yAdjustment = -this.transform.GetComponent<RectTransform>().rect.height / 2;
+            for (int i = 0; i < starNumber; i++)
+            {
+                Instantiate(starPicture, new Vector2(this.transform.position.x + xAdjustment, this.transform.position.y + yAdjustment), Quaternion.identity, this.transform);
+                xAdjustment += this.transform.GetComponent<RectTransform>().rect.width / 4;
+            }
+        }
+    }
+
+    public void LoadStage()
+    {
+        SceneManager.LoadScene(stage);
+    }
 }
